b1f40413ded72467eb6c798f08c8fd5f6f28e48e
Kevin Holmes <kevin.holmes@gmail.com>
2023-09-26 14:09:06 -0400

    feat: import apex domain pfx-type cert and utilize for azure cdn custom domain ep (#21)

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/pulumi/pulumi-azuread/sdk/v5 v5.42.0
 	github.com/pulumi/pulumi-github/sdk/v5 v5.19.0
 	github.com/pulumi/pulumi/sdk/v3 v3.85.0
+	software.sslmate.com/src/go-pkcs12 v0.2.1
 )
 
 require (
@@ -78,12 +79,12 @@ require (
 	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	github.com/zclconf/go-cty v1.12.1 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
-	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/term v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/term v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230706204954-ccb25ca9f130 // indirect
 	google.golang.org/grpc v1.57.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
--- scripts/sops.sh
+++ scripts/sops.sh
@@ -1,23 +1,48 @@
-#!/bin/bash
+#!/bin/bash -x
+
+# encrypt or decrypt all tls assets at rest in src control
+# see project root's .sops.yaml for SOPS defaults
 
 TLS_FILE_LOCATION=./assets/tls
-FILES=("$TLS_FILE_LOCATION"/*)
+DEC_POSTFIX="dec"
+ENC_POSTFIX="enc"
+
+SOPS_SUCCESS="SOPS operation successful"
+SOPS_FAILURE="SOPS operation failed, bailing out before destroying any files"
 
 if [ -z "$1" ]; then
  echo "requires either 'encrypt or 'decrypt' argument e.g.: ./sops.sh encrypt"
 fi
 
 if [ "$1" == "encrypt" ]; then
+  FILES=("$TLS_FILE_LOCATION"/*".$DEC_POSTFIX")
   for f in "${FILES[@]}"
   do
-    echo encrypting "$f"
-    sops --encrypt --in-place "$f"
+    newName=${f//dec/enc}
+    echo encrypting "$f" to "$newName"
+    if sops --output "$newName" --encrypt "$f"
+    then
+      echo "$SOPS_SUCCESS"
+      rm "$f"
+    else
+      echo "$SOPS_FAILURE"
+      exit 1
+    fi
   done
 elif [ "$1" == "decrypt" ]; then
+  FILES=("$TLS_FILE_LOCATION"/*.pfx".$ENC_POSTFIX")
   for f in "${FILES[@]}"
   do
-    echo decrypting "$f"
-    sops --decrypt --in-place "$f"
+    newName=${f//enc/dec}
+    echo decrypting "$f" to "$newName"
+    if sops --output "$newName" --decrypt "$f"
+    then
+      echo "$SOPS_SUCCESS"
+      rm "$f"
+    else
+      echo "$SOPS_FAILURE"
+      exit 1
+    fi
   done
 else
   echo "FATAL: unknown argument: $1"
