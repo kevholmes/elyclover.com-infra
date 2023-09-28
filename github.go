e5a1e55eb11dee999c70232fd699d8c4c8420483
Kevin Holmes <kevin.holmes@gmail.com>
2023-09-28 16:53:53 -0400

    fix: allow dependabot access to dev azure SP secret token

--- github.go
+++ github.go
@@ -12,7 +12,7 @@ import (
 )
 
 func exportDeployEnvDataToGitHubRepo(ctx *pulumi.Context, cfg *config.Config, sp ServicePrincipalEnvelope, rg *resources.ResourceGroup, sa *storage.StorageAccount, cdnprof *cdn.Profile, ep *cdn.Endpoint) (err error) {
-	// validate repo
+	// Validate repo
 	githubConfig := config.New(ctx, "github")
 	repoPath := fmt.Sprintf("%s/%s", githubConfig.Require("owner"), cfg.Require("ghAppSrcRepo"))
 	repo, err := github.LookupRepository(ctx, &github.LookupRepositoryArgs{
@@ -22,7 +22,7 @@ func exportDeployEnvDataToGitHubRepo(ctx *pulumi.Context, cfg *config.Config, sp
 		return err
 	}
 
-	// create deployment environment using stack name (env)
+	// Create deployment environment using stack name (env)
 	env := ctx.Stack()
 	repoEnv, err := github.NewRepositoryEnvironment(ctx, env, &github.RepositoryEnvironmentArgs{
 		Repository:  pulumi.String(repo.Name),
@@ -32,7 +32,7 @@ func exportDeployEnvDataToGitHubRepo(ctx *pulumi.Context, cfg *config.Config, sp
 		return err
 	}
 
-	// create Actions Deployment Environment Secret for Azure SP that will be deploying via Actions workflows
+	// Create Actions Deployment Environment Secret for Azure SP that will be deploying via Actions workflows
 	// https://github.com/Azure/login#configure-a-service-principal-with-a-secret
 	secretsVars := map[string]pulumi.StringInput{
 		"CLIENT_SECRET": sp.ServicePrincipalPass.Value,
@@ -47,8 +47,19 @@ func exportDeployEnvDataToGitHubRepo(ctx *pulumi.Context, cfg *config.Config, sp
 		if err != nil {
 			return err
 		}
+		// This permits PRs submitted by Dependabot (e.g. when bumping project dependencies to newer versions)
+		// to have access to the dev stack environment's Azure SP client secret token.
+		// This allows the Dependabot PR to be treated just like a user-submitted (chore-like) PR to bump the dependency
+		// The outcome here is that the Dependabot submitted PR gets built and deployed just like any other.
+		if ctx.Stack() == "dev" {
+			_, err = github.NewDependabotSecret(ctx, k, &github.DependabotSecretArgs{
+				Repository:     pulumi.String(repo.Name),
+				SecretName:     pulumi.String(k),
+				PlaintextValue: v,
+			})
+		}
 	}
-	// create Actions Deployment Environment Variables to be used in Actions CI/CD workflows
+	// Create Actions Deployment Environment Variables to be used in Actions CI/CD workflows
 	actionsVars := map[string]pulumi.StringInput{
 		"AZ_CDN_ENDPOINT":     ep.Name,
 		"AZ_CDN_PROFILE_NAME": cdnprof.Name,
