05b7b50aa980ae8595b184e81f5dae9d83db5dd9
Kevin Holmes <kevin.holmes@gmail.com>
2023-12-06 13:58:58 -0500

    fix!: common structs, readability (#79)

--- github.go
+++ github.go
@@ -3,19 +3,16 @@ package main
 import (
 	"fmt"
 
-	"github.com/pulumi/pulumi-azure-native-sdk/cdn/v2"
-	"github.com/pulumi/pulumi-azure-native-sdk/resources/v2"
-	"github.com/pulumi/pulumi-azure-native-sdk/storage/v2"
 	"github.com/pulumi/pulumi-github/sdk/v5/go/github"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
-func exportDeployEnvDataToGitHubRepo(ctx *pulumi.Context, cfg *config.Config, sp ServicePrincipalEnvelope, rg *resources.ResourceGroup, sa *storage.StorageAccount, cdnprof *cdn.Profile, ep *cdn.Endpoint) (err error) {
+func (pr *projectResources) exportDeployEnvDataToGitHubRepo() (err error) {
 	// Validate repo
-	githubConfig := config.New(ctx, "github")
-	repoPath := fmt.Sprintf("%s/%s", githubConfig.Require("owner"), cfg.Require("ghAppSrcRepo"))
-	repo, err := github.LookupRepository(ctx, &github.LookupRepositoryArgs{
+	githubConfig := config.New(pr.pulumiCtx, "github")
+	repoPath := fmt.Sprintf("%s/%s", githubConfig.Require("owner"), pr.cfgKeys.ghAppSrcRepo)
+	repo, err := github.LookupRepository(pr.pulumiCtx, &github.LookupRepositoryArgs{
 		FullName: &repoPath,
 	})
 	if err != nil {
@@ -23,8 +20,8 @@ func exportDeployEnvDataToGitHubRepo(ctx *pulumi.Context, cfg *config.Config, sp
 	}
 
 	// Create deployment environment using stack name (env)
-	env := ctx.Stack()
-	repoEnv, err := github.NewRepositoryEnvironment(ctx, env, &github.RepositoryEnvironmentArgs{
+	env := pr.cfgKeys.envKey
+	repoEnv, err := github.NewRepositoryEnvironment(pr.pulumiCtx, env, &github.RepositoryEnvironmentArgs{
 		Repository:  pulumi.String(repo.Name),
 		Environment: pulumi.String(env),
 	})
@@ -35,10 +32,10 @@ func exportDeployEnvDataToGitHubRepo(ctx *pulumi.Context, cfg *config.Config, sp
 	// Create Actions Deployment Environment Secret for Azure SP that will be deploying via Actions workflows
 	// https://github.com/Azure/login#configure-a-service-principal-with-a-secret
 	secretsVars := map[string]pulumi.StringInput{
-		"CLIENT_SECRET": sp.ServicePrincipalPass.Value,
+		"CLIENT_SECRET": pr.svcPrincipals.cicd.ServicePrincipalPass.Value,
 	}
 	for k, v := range secretsVars {
-		_, err = github.NewActionsEnvironmentSecret(ctx, k, &github.ActionsEnvironmentSecretArgs{
+		_, err = github.NewActionsEnvironmentSecret(pr.pulumiCtx, k, &github.ActionsEnvironmentSecretArgs{
 			Repository:     pulumi.String(repo.Name),
 			SecretName:     pulumi.String(k),
 			Environment:    repoEnv.Environment,
@@ -51,8 +48,8 @@ func exportDeployEnvDataToGitHubRepo(ctx *pulumi.Context, cfg *config.Config, sp
 		// to have access to the dev stack environment's Azure SP client secret token.
 		// This allows the Dependabot PR to be treated just like a user-submitted (chore-like) PR to bump the dependency
 		// The outcome here is that the Dependabot submitted PR gets built and deployed just like any other.
-		if ctx.Stack() == "dev" {
-			_, err = github.NewDependabotSecret(ctx, k, &github.DependabotSecretArgs{
+		if pr.cfgKeys.envKey == DEV {
+			_, err = github.NewDependabotSecret(pr.pulumiCtx, k, &github.DependabotSecretArgs{
 				Repository:     pulumi.String(repo.Name),
 				SecretName:     pulumi.String(k),
 				PlaintextValue: v,
@@ -61,16 +58,16 @@ func exportDeployEnvDataToGitHubRepo(ctx *pulumi.Context, cfg *config.Config, sp
 	}
 	// Create Actions Deployment Environment Variables to be used in Actions CI/CD workflows
 	actionsVars := map[string]pulumi.StringInput{
-		"AZ_CDN_ENDPOINT":     ep.Name,
-		"AZ_CDN_PROFILE_NAME": cdnprof.Name,
-		"AZ_RESOURCE_GROUP":   rg.Name,
-		"AZ_STORAGE_ACCT":     sa.Name,
-		"CLIENT_ID":           sp.ServicePrincipal.ClientId,
-		"SUBSCRIPTION_ID":     pulumi.String(cfg.Require("AzSubScriptionIdWeb")),
-		"TENANT_ID":           pulumi.String(cfg.Require("AzTenantId")),
+		"AZ_CDN_ENDPOINT":     pr.webCdnEp.Name,
+		"AZ_CDN_PROFILE_NAME": pr.webCdnProfile.Name,
+		"AZ_RESOURCE_GROUP":   pr.webResourceGrp.Name,
+		"AZ_STORAGE_ACCT":     pr.webStorageAccount.Name,
+		"CLIENT_ID":           pr.svcPrincipals.cicd.ServicePrincipal.ClientId,
+		"SUBSCRIPTION_ID":     pulumi.String(pr.thisAzureSubscription.SubscriptionId),
+		"TENANT_ID":           pulumi.String(pr.cfgKeys.thisAzureTenantId),
 	}
 	for k, v := range actionsVars {
-		_, err = github.NewActionsEnvironmentVariable(ctx, k, &github.ActionsEnvironmentVariableArgs{
+		_, err = github.NewActionsEnvironmentVariable(pr.pulumiCtx, k, &github.ActionsEnvironmentVariableArgs{
 			Environment:  repoEnv.Environment,
 			Repository:   pulumi.String(repo.Name),
 			VariableName: pulumi.String(k),
--- utils.go
+++ utils.go
@@ -5,9 +5,8 @@ import (
 	"strings"
 
 	"github.com/pulumi/pulumi-azure-native-sdk/storage/v2"
+	"github.com/pulumi/pulumi-azure/sdk/v5/go/azure/core"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-
-	//"golang.org/x/crypto/pkcs12"
 	pkcs12 "software.sslmate.com/src/go-pkcs12"
 )
 
@@ -32,5 +31,26 @@ func loadFileFromDisk(path string) (data []byte, err error) {
 	if err != nil {
 		return data, err
 	}
+	return
+}
+
+// consider iterating over pr.cfgKeys with a k:v map to load these in if list continues to grow
+func (pr *projectResources) initConfigKeys() (err error) {
+	pr.cfgKeys.projectKey = pr.pulumiCtx.Project()
+	pr.cfgKeys.envKey = pr.pulumiCtx.Stack()
+	pr.cfgKeys.siteKey = pr.cfg.Require("siteKey")
+	pr.cfgKeys.dnsResourceGrp = pr.cfg.Require("dnsResourceGroup")
+	pr.cfgKeys.dnsLookupZone = pr.cfg.Require("dnsZoneName")
+	pr.cfgKeys.ghAppSrcRepo = pr.cfg.Require("ghAppSrcRepo")
+	pr.cfgKeys.thisAzureTenantId = pr.cfg.Require("AzTenantId")
+	pr.cfgKeys.dnsRecordTTL = pr.cfg.Require("dnsRecordTTL")
+	if pr.cfgKeys.envKey == PROD {
+		pr.cfgKeys.cdnAzureId = pr.cfg.Require("Microsoft.AzureFrontDoor-Cdn")
+		pr.cfgKeys.kvAzureSubscription = pr.cfg.Require("keyVaultAzureSubscription")
+		pr.cfgKeys.kvAzureResourceGrp = pr.cfg.Require("keyvaultResourceGroup")
+		pr.cfgKeys.kvAzureName = pr.cfg.Require("keyVaultName")
+	}
+	pr.thisAzureSubscription, err = core.LookupSubscription(pr.pulumiCtx, nil)
+
 	return
 }
